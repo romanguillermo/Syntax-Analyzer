56253e45f9ee4f1fba3b856c9b0faea0b943254c
Guillermo Roman <roman.guillermo52@gmail.com>
2024-04-24 23:33:19 -0700

    Output implemented in parse method using steps list to track steps,stack,input,action for every step of loop. Parser completed.

--- parser_program.py
+++ parser_program.py
@@ -1,23 +1,25 @@
-input_string1 = "(id+id)*id$"
+input_string1 = "(id+id)$"
 input_string2 = "id*id$"
 input_string3 = "(id*)$"
 
 
 class Parser:
     def __init__(self, input):
+        self.input_const = input  # For output
         self.input = input
-        self.position = 0
-        self.current_char = self.input[self.position] if self.input else None
-        self.stack = [("$", 0)] # Stack of tuples: (symbol,state)
+        self.current_char = self.input[0] if self.input else None
+        self.stack = [("$", 0)]  # Stack of tuples: (symbol,state)
         self.state = 0
-        self.rules = {
-            1: ('E', ['E', '+', 'T']),
-            2: ('E', ['T']),
-            3: ('T', ['T', '*', 'F']),
-            4: ('T', ['F']),
-            5: ('F', ['(', 'E', ')']),
-            6: ('F', ['id'])
-        }
+        self.rules = (
+            {  # production rules: lhs = first item of tuple, rhs = second item of tuple
+                1: ("E", ["E", "+", "T"]),
+                2: ("E", ["T"]),
+                3: ("T", ["T", "*", "F"]),
+                4: ("T", ["F"]),
+                5: ("F", ["(", "E", ")"]),
+                6: ("F", ["id"]),
+            }
+        )
         self.parsing_table = {
             0: {"id": "S5", "(": "S4", "E": 1, "T": 2, "F": 3},
             1: {"+": "S6", "$": "accept"},
@@ -33,40 +35,56 @@ class Parser:
             11: {"+": "R5", "*": "R5", ")": "R5", "$": "R5"},
         }
 
-    def advance(self): # Instead of advance, maybe consume for input string to be output?
-        self.position += 1
-        self.current_char = (self.input[self.position] if self.position < len(self.input) else None)
+    def next_char(self):
+        """Removes first character from input and sets current char to next character (first of updated input)"""
+        self.input = self.input[1:]
+        self.current_char = self.input[0] if self.input else None
 
     def parse(self):
-        """Parse the input string using the parsing table and rules"""
+        """Parse the input string using the parsing table and rules and display output"""
+        steps = []  # For tracking the steps of the parser for output table
+        step = 1
         while True:
             # State = top of stack, second item in tuple
             self.state = int(self.stack[-1][1])
 
             if self.current_char == "i":
-                self.advance()
+                self.next_char()
                 self.current_char = "id"
 
             # Lookup action in parsing table given state and current input symbol
-            if self.current_char in self.parsing_table[self.state]: # If action exists for current state and symbol
+            if (self.current_char in self.parsing_table[self.state]):  # If action exists for current state and symbol
                 action = self.parsing_table[self.state][self.current_char]
-                # Call corresponding action function
+                steps.append((step, self.stack.copy(), self.input, action)) # Append step to steps list before action is taken
                 if action[0] == "S":
                     state = int(action[1:])
                     self.shift(state)
-                    self.advance()
+                    self.next_char()
                 elif action[0] == "R":
                     rule = int(action[1:])
                     self.reduce(rule)
                 elif action == "accept":
                     self.accepted = True
                     break
-                else:
-                    self.accepted = False
-                    break
             else:
                 self.accepted = False
                 break
+
+            step += 1
+
+        # Output table
+        print(f"Input: {self.input_const}")
+        print("Stack:")
+        print(f"{'Step':<5}\t{'Stack':<30}\t{'Input':<20}\t{'Action':<4}")
+        print("-" * 80)
+        for step, stack, input, action in steps:
+            print(
+                f"{step:<5}\t{' '.join([f'{s[0]} {s[1]}' for s in stack]):<30}\t{input:20}\t{action:4}"
+            )
+        if self.accepted:
+            print("String is accepted")
+        else:
+            print("String is not accepted")
 
     def shift(self, state):
         """Shift the current symbol and the next state to stack"""
@@ -75,7 +93,7 @@ class Parser:
     def reduce(self, rule):
         """Reduce the stack by rhs of corresponding rule and replace with lhs and next state"""
         # Left hand side and right hand side to the equivalent lhs, rhs of tuple in production rule
-        lhs,rhs = self.rules[rule]
+        lhs, rhs = self.rules[rule]
         # Pop the stack the number of symbols in the rhs of rule
         for symbols in range(len(rhs)):
             self.stack.pop()
@@ -83,20 +101,7 @@ class Parser:
         goto_state = self.parsing_table[self.stack[-1][1]][lhs]
         self.stack.append((lhs, goto_state))
 
-    def output(self):
-        """Print output of stack"""
-        
-        print(f"Input: {self.input}")
-
-        print("Stack:")
-        # print stack/input/action table
-
-        if self.accepted:
-            print("Output: String is accepted")
-        else: 
-            print("Output: String is not accepted")
 
 # Usage
-parser = Parser(input_string1)
-parser.parse()
-parser.output()
+example = Parser(input_string1)
+example.parse()
